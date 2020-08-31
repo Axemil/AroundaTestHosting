f73f881a8e654ae20b0edc69a76724ef840de104
Vlad <blxnsh@gmail.com>
2020-09-01 00:03:58 +0300

    Revert "Update index.js"
    This reverts commit b56ff9fca1a21fd87f562081ffbe6377c9b9e228.

--- src/components/sections/Layout/index.js
+++ src/components/sections/Layout/index.js
@@ -55,7 +55,7 @@ const Layout = ({ children, isMobile }) => {
 	const {pathname} = useRouter();
 	
 	Router.events.on('routeChangeComplete', () => { {
-		window.scrollTo(0, 0);
+		window.scrollY != 0 && window.scrollTo(0, 0);
 	} })
 
 	return (
