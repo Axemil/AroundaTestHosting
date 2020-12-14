e1225cb8c7bdffa80890352fea3b3e2556c8913f
Axemil <46081813+Axemil@users.noreply.github.com>
2020-12-14 14:19:51 +0200

    font preloader with nextJS

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document"
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -10,7 +10,9 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-                <script dangerouslySetInnerHTML={{__html: `;(function(w, d, s, l, i) {
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `;(function(w, d, s, l, i) {
                     w[l] = w[l] || []
                     w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" })
                     var f = d.getElementsByTagName(s)[0],
@@ -19,15 +21,107 @@ class MyDocument extends Document {
                     j.async = true
                     j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl
                     f.parentNode.insertBefore(j, f)
-                })(window, document, "script", "dataLayer", "GTM-55T4HZL")`}}>
-                </script>
+                })(window, document, "script", "dataLayer", "GTM-55T4HZL")`
+            }}
+          ></script>
 
-                <link rel="icon" type="image/x-icon" href="/favicon_arounda.ico" />
-                <link rel="icon" type="image/x-icon" sizes="16x16" href="/favicon-16x16.ico" />
-                <link rel="icon" type="image/x-icon" sizes="32x32" href="/favicon-32x32.ico" />
-                <link rel="icon" type="image/x-icon" sizes="48x48" href="/favicon-48x48.ico" />
+          <link
+            rel="preload"
+            href="/fonts/Gordita-Bold.ttf"
+            as="font"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/Gordita-Bold.woff"
+            as="font"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/Gordita-Bold.woff2"
+            as="font"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/Gordita-Light.ttf"
+            as="font"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/Gordita-Light.woff"
+            as="font"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/Gordita-Light.woff2"
+            as="font"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/Gordita-Medium.ttf"
+            as="font"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/Gordita-Medium.woff"
+            as="font"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/Gordita-Medium.woff2"
+            as="font"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/Gordita-Regular.ttf"
+            as="font"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/Gordita-Regular.woff"
+            as="font"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/Gordita-Regular.woff2"
+            as="font"
+            crossOrigin="anonymous"
+          />
 
-                <script id="mcjs" dangerouslySetInnerHTML={{__html: `!(function(c, h, i, m, p) {
+          <link rel="icon" type="image/x-icon" href="/favicon_arounda.ico" />
+          <link
+            rel="icon"
+            type="image/x-icon"
+            sizes="16x16"
+            href="/favicon-16x16.ico"
+          />
+          <link
+            rel="icon"
+            type="image/x-icon"
+            sizes="32x32"
+            href="/favicon-32x32.ico"
+          />
+          <link
+            rel="icon"
+            type="image/x-icon"
+            sizes="48x48"
+            href="/favicon-48x48.ico"
+          />
+
+          <script
+            id="mcjs"
+            dangerouslySetInnerHTML={{
+              __html: `!(function(c, h, i, m, p) {
                     ;(m = c.createElement(h)),
                     (p = c.getElementsByTagName(h)[0]),
                     (m.async = 1),
@@ -37,8 +131,9 @@ class MyDocument extends Document {
                     document,
                     "script",
                     "https://chimpstatic.com/mcjs-connected/js/users/a21e974ea6ac7b0b696b05da5/f66e8437304041aa54ab14c2a.js"
-                )`}}>
-                </script>
+                )`
+            }}
+          ></script>
         </Head>
         <body>
           <Main />
